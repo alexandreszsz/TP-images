e1736cb4db6f676273dc0f60578cd66d521c74ed
Alexandre Szulman <alexandre.szulman@etu.minesparis.psl.eu>
2026-09-08 10:17:29 +0200

    partie 1, manipulation d'images

--- sheet1.py
+++ sheet1.py
@@ -1,6 +1,17 @@
 import numpy as np
 from matplotlib import pyplot as plt
-t=np.zeros((91,91,3))
-t=np.array(t,dtype=np.int8)
+t=np.empty((91,91,3))
+t=np.array(t,dtype=np.int16) #le tableau est ici noir
+t[:]=255 #le tableau devient blanc
+t[:]=0 #A nouveau noir pour faire du vert dans la ligne suivante
+t[:,:,1]=255 #le tableau devient vert
+
+#print((t[0,0],t[90,90])) # On affiche les valeurs du premier pixel et du dernier pixel
+t[:]=0 #A nouveau noir
+
+for i in range(10):
+    t[:,i*10,2]=255
+    t[i*10,:,2]=255 #Quadrillage de bleu
+
 plt.imshow(t)
-plt.show
+plt.show()
